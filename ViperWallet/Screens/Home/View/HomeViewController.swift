9284fa6145dc34957022db4232e2b0f2c0e6d0ae
mahmutgazidogan <m.gazi.dogann@gmail.com>
2023-10-15 18:46:13 +0300

    folders created

---
+++ ViperWallet/Screens/Home/View/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  ViperWallet
+//
+//  Created by Mahmut Gazi Doğan on 15.10.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .cyan
+    }
+    
+
+    
+
+}
